aef24f5d01301feeff3a3c4473f4101a3ea57d27
stargazer1823 <noah.peterson@drake.edu>
2022-09-08 22:19:55 -0500

    add mutual Compression

--- LZCompression.py
+++ LZCompression.py
@@ -25,6 +25,7 @@ def lzEndoder(s):
             curr = ""
             currenc = ""
             i = i +1
+            #print(dic)
     return(oupt)
 
 
@@ -43,6 +44,7 @@ def lzDecoder(lst):
             dec = ele
         oupt = oupt + dec
         dic[len(dic)] = dec
+        #print(dic)
     return(oupt)
 
 
@@ -52,27 +54,40 @@ def zipper(s1,s2):
     i = 0
     while i < len(s1):
         oupt = oupt + s1[i]
-        if i <len(S2):
+        if i <len(s2):
             oupt = oupt + s2[i]
-        i ++
+        i = i+ 1
     while i< len(s2):
         oupt = oupt + s2[i]
-        i ++
+        i = i+ 1
     return(oupt)
 
 def lzCompression_ratio(s):
-    return(len(lzEndoder(s))/len(s))
+    return(len("".join(lzEndoder(s)))/len(s))
 
-def Mutual_Compression_ratio(s1,s2)
+def Mutual_Compression_ratio(s1,s2):
     s12 = zipper(s1,s2)
     return(lzCompression_ratio(s1)+lzCompression_ratio(s2)-lzCompression_ratio(s12))
     
+#==============Testing LZ Algorithum ============================================
+#s = "AABABBABBAABA"
+#s = "Hello My name is Noah Nice To meet you"
+#enc = lzEndoder(s)
+#print("Length of string:" + str(len(s)))
+#lenenc = 0
+#for ele in enc:
+#    lenenc = lenenc + len(ele)
+#print("Length of encoded string:" + str(lenenc))
+#print(lzDecoder(enc))
 
-s = "AABABBABBAABA"
-enc = lzEndoder(s)
-print("Length of string:" + str(len(s)))
-lenenc = 0
-for ele in enc:
-    lenenc = lenenc + len(ele)
-print("Length of encoded string:" + str(lenenc))
-print(lzDecoder(enc))
+#=======================Testing mutual Compression Ratio ===========================
+#s1 = "1101001001101"
+#s2 = "1101001001101"
+
+s1 = "101000101101111111000011011100011001010001010000101111000110"
+s2 = "001010101110000100000111110000100001111000001110000111000100"
+print(lzCompression_ratio(s1))
+print(lzCompression_ratio(s2))
+s12 = zipper(s1,s2)
+print(lzCompression_ratio(s12))
+print(Mutual_Compression_ratio(s1,s2))
